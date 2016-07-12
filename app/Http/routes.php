1c2bc6f73decc1c4fdadc53740c6932672f400ed
1124ritesh@gmail.com <1124ritesh@gmail.com>
2016-07-12 15:16:38 +0530

    firstcommit

---
+++ app/Http/routes.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Support\Facades\App;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/bridge', function() {
+    $pusher = App::make('pusher');
+
+    $pusher->trigger( 'test-channel',
+                      'test-event', 
+                      array('text' => 'Preparing the Pusher Laracon.eu workshop!'));
+
+    return view('welcome');
+});
+Route::controller('notifications', 'NotificationController');
+
+Route::controller('chat', 'ChatController');
+//Route::auth();
+
+Route::get('/home', 'HomeController@index');
+Route::get('auth/github', 'Auth\AuthController@redirectToProvider');
+Route::get('auth/github/callback', 'Auth\AuthController@handleProviderCallback');
+Route::controller('activities', 'ActivityController');
